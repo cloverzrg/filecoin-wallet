3b73f213060aacd854fab33b4cceb2cc993fe7ba
Zhu Rengen <zhurungen@wecash.net>
2021-06-30 18:57:38 +0800

    add env

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN apt-get update && apt-get install -y --no-cache ca-certificates tzdata
 ADD go-bin /app/fil-wallet
 EXPOSE 80
 ENV ENDPOINT https://calibration.node.glif.io
+ENV TOKEN ''
 VOLUME /app/data
 CMD ["/app/fil-wallet"]
--- config/config.go
+++ config/config.go
@@ -1,4 +1,17 @@
 package config
 
+import "os"
 
-const Endpoint = "https://calibration.node.glif.io"
+var Endpoint = "https://calibration.node.glif.io"
+var Token = ""
+
+func init() {
+	endpoint := os.Getenv("ENDPOINT")
+	if endpoint != "" {
+		Endpoint = endpoint
+	}
+	token := os.Getenv("TOKEN")
+	if token != "" {
+		Token = token
+	}
+}
